c87241972b3cea0c0443816edd1e4a39cb7e077e
Khawlah Khalid <khawlah1920@hotmail.com>
2024-01-30 07:23:29 +0300

    add items array in category

--- Sources/GroceryAppSharedDTO24/CategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO24/CategoryResponseDTO.swift
@@ -11,11 +11,13 @@ public struct CategoryResponseDTO: Codable{
     public var id: UUID
     public var title: String
     public var colorHex: String
+    public var items: [ItemResponseDTO]? = []
     
-    public init(id: UUID, title: String, colorHex: String) {
+    public init(id: UUID, title: String, colorHex: String, items: [ItemResponseDTO]? = [] ) {
         self.id = id
         self.title = title
         self.colorHex = colorHex
+        self.items = items
     }
     
 }
